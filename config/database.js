1862e8ab42b01307d6845aaf82a0c049eb6e9b77
Shinoo <s6203051623081@email.kmutnb.ac.th>
2022-09-22 21:55:08 +0700

    add database

---
+++ config/database.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient
+const url = process.env.DB_URL
+
+var _db
+
+module.exports = {
+  connectToServer: function( callback ) {
+    MongoClient.connect( url,  { useNewUrlParser: true }, function( err, client ) {
+      _db  = client.db('bookstore')
+      console.log("connected to database")
+      return callback( err )
+    })
+  },
+
+  getDb: function() {
+    return _db
+  }
+}
