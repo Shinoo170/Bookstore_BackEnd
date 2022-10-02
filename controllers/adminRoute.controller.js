2a13c81324b1ad796d8544dc903deee0fb0956a4
Shinoo <s6203051623081@email.kmutnb.ac.th>
2022-10-02 14:48:56 +0700

    fixed aws storage (#10)

--- controllers/adminRoute.controller.js
+++ controllers/adminRoute.controller.js
@@ -62,8 +62,8 @@ function uploadImage(files) {
     return new Promise(async (resolve, reject) => {
         try{
             const s3 = new AWS.S3({
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+                accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
             })
             var imgURL = []
             const promise = files.map(async (imageData) => {
