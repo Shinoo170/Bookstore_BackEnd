fc9a7e2e17e5f53056c024f4951281050470791b
Shinoo <s6203051623081@email.kmutnb.ac.th>
2022-11-17 16:05:12 +0700

    change mongodb connection

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,16 +1,13 @@
 // [ connect to database ]
-const mongoUtil = require('../config/database')
+const { MongoClient } = require('mongodb')
 const ObjectId = require('mongodb').ObjectId
-mongoUtil.connectToServer(function(err, client){
-    if (err) console.log(err)
-})
 
 exports.getAllSeries = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
-        mongoUtil.connectToServer(async function(err, client){
-            if (err) console.log(err)
-            const db = mongoUtil.getDb()
-            await db.collection('series')
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const result = await db.collection('series')
             .find({})
             .project({
                 _id: 0,
@@ -30,20 +27,22 @@ exports.getAllSeries = async (req, res) => {
                     totalNovel: 1,
                     totalOther: 1,
                 }
-            }).toArray( (err, result) => {
-                if(err) res.status(400).send({ message: 'Cannot connect to database'})
-                res.send(result)
-            })
-        })
-    }catch(err){
-        res.status(400).send({message: 'Error to get data', err})
+            }).toArray()
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.getSeriesDetails = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const params = req.params
-        const db = mongoUtil.getDb()
         const seriesData = await db.collection('series').findOne({ seriesId: parseInt(params.seriesId) })
         const manga = await db.collection('products')
             .find({ _id: { $in: seriesData.products.mangaId} })
@@ -83,14 +82,19 @@ exports.getSeriesDetails = async (req, res) => {
         } else {
             res.status(400).send({ message: 'No series found!' })
         }
-    }catch(err){
-        res.status(400).send({message: 'Error to get data', err: err.message })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.getProductInSeries = async (req, res) => {
-    try {
-        const db = mongoUtil.getDb()
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const query = req.query
         const target = query.category.toLowerCase() + 'Id'
         const listProductId = await db.collection('series').findOne({seriesId: parseInt(query.seriesId)},{
@@ -107,14 +111,19 @@ exports.getProductInSeries = async (req, res) => {
         res.status(200).send(data)
     } catch (err) {
         console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.getProduct = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const params = req.params
         const query = req.query
-        const db = mongoUtil.getDb()
         const target = query.category.toLowerCase() + 'Id'
         const productDetails = await db.collection('products').findOne({ url: params.productURL })
 
@@ -149,160 +158,187 @@ exports.getProduct = async (req, res) => {
 
         if( productDetails === null ) res.status(404).send()
         else res.status(200).send({productDetails, seriesDetails, otherProducts , similarProducts})
-    }catch(err){
+    } catch (err) {
         console.log(err)
-        res.status(400).send({message: 'Error to get data', err})
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.getLatestSeries = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
-        mongoUtil.connectToServer(async function(err, client){
-            if (err) console.log(err)
-            const db = mongoUtil.getDb()
-            const data = await db.collection('series').find({}).project({
-                _id: 0,
-                seriesId: 1,
-                title: 1,
-                author: 1,
-                illustrator: 1,
-                publisher: 1,
-                genres: 1,
-                img: 1,
-                score: 1,
-                addDate: 1,
-                lastModify: 1,
-                status: 1,
-                products: {
-                    totalManga: 1,
-                    totalNovel: 1,
-                    totalOther: 1,
-                }
-            }).sort({lastModify: -1}).toArray()
-            if(data){
-                res.status(200).send(data)
-            }else{
-                res.status(400).send({message: 'No product found!', err})
-            } 
-        })
-    }catch(err){
-        console.log(err)
-        res.status(400).send({message: 'Error to get data', err})
-    }
-}
-
-exports.getLatestProduct = async (req, res) => {
-    try{
-        const db = mongoUtil.getDb()
-        const data = await db.collection('products').find({}).limit(10).sort({productId: -1}).toArray()
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const data = await db.collection('series').find({}).project({
+            _id: 0,
+            seriesId: 1,
+            title: 1,
+            author: 1,
+            illustrator: 1,
+            publisher: 1,
+            genres: 1,
+            img: 1,
+            score: 1,
+            addDate: 1,
+            lastModify: 1,
+            status: 1,
+            products: {
+                totalManga: 1,
+                totalNovel: 1,
+                totalOther: 1,
+            }
+        }).sort({lastModify: -1}).toArray()
         if(data){
             res.status(200).send(data)
         }else{
             res.status(400).send({message: 'No product found!', err})
-        }
-    }catch(err){
-        res.status(400).send({message: 'Error to get data', err})
+        } 
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
-exports.getMostSoldProduct = async (req, res) => {
+exports.getLatestProduct = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
-        mongoUtil.connectToServer(async function(err, client){
-            if(err) res.status(500).send({message: 'Cannot connect to database', err})
-            const db = mongoUtil.getDb()
-            const data = await db.collection('products').find({}).limit(10).sort({sold: -1}).toArray()
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const data = await db.collection('products').find({}).limit(10).sort({productId: -1}).toArray()
             if(data){
                 res.status(200).send(data)
             }else{
                 res.status(400).send({message: 'No product found!', err})
             }
-        })
-    }catch(err){
-        res.status(500).send({message: 'Server error', err})
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
+    }
+}
+
+exports.getMostSoldProduct = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const data = await db.collection('products').find({}).limit(10).sort({sold: -1}).toArray()
+        if(data){
+            res.status(200).send(data)
+        }else{
+            res.status(400).send({message: 'No product found!', err})
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 
 // Genres
 exports.getAllGenres = async (req, res) => {
+    const client = new MongoClient(process.env.MONGODB_URI)
     try{
-        const db = mongoUtil.getDb()
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const data = await db.collection('globalData').findOne({ field: 'genres'}, { projection: { _id: 0, data: 1}})
         res.send( data.data.sort() )
-    } catch(err) {
-        res.status(400).send({ message: 'error', err})
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 // Bookmark
 exports.isUserSubscribe = async (req, res) => {
-    try {
-        mongoUtil.connectToServer(async function(err, client){
-            if (err) res.status(500).send({message: 'Cannot connect to database'})
-            var db = mongoUtil.getDb()
-            const seriesId = parseInt(req.query.seriesId)
-            const user_id = new ObjectId(req.user_id)
-            const data = await db.collection('series').findOne({ seriesId, subscribe: [user_id] })
-            if(data){
-                res.status(200).send(true)
-            } else {
-                res.status(200).send(false)
-            }
-        })
-    } catch (error) {
-        res.status(500).send({message: 'This service is not available'})
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const seriesId = parseInt(req.query.seriesId)
+        const user_id = new ObjectId(req.user_id)
+        const data = await db.collection('series').findOne({ seriesId, subscribe: [user_id] })
+        if(data){
+            res.status(200).send(true)
+        } else {
+            res.status(200).send(false)
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.addSubscriber = async (req, res) => {
-    try {
-        mongoUtil.connectToServer(async function(err, client){
-            if (err) res.status(500).send({message: 'Cannot connect to database'})
-            var db = mongoUtil.getDb()
-            const seriesId = parseInt(req.query.seriesId)
-            const _id = new ObjectId(req.user_id)
-            db.collection('series').updateOne({ seriesId }, {
-                $push: {
-                    subscribe: _id,
-                }
-            }, (err, result) => {
-                if(err || result.matchedCount != 1) return res.status(400).send({message: 'Error to add bookmark'})
-                res.status(201).send(true)
-            })
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const seriesId = parseInt(req.query.seriesId)
+        const _id = new ObjectId(req.user_id)
+        const result = await db.collection('series').updateOne({ seriesId }, {
+            $push: {
+                subscribe: _id,
+            }
         })
-    } catch (error) {
-        res.status(500).send({message: 'This service is not available'})
+        if(result.modifiedCount === 1){
+            res.status(201).send(true)
+        } else {
+            res.status(500).send({message: 'Cannot subscribe', err})
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.removeSubscriber = async (req, res) => {
-    try {
-        mongoUtil.connectToServer(function(err, client){
-            if (err) res.status(500).send({message: 'Cannot connect to database'})
-            var db = mongoUtil.getDb()
-            const seriesId = parseInt(req.query.seriesId)
-            const _id = new ObjectId(req.user_id)
-            db.collection('series').updateOne({ seriesId }, {
-                $pull: {
-                    subscribe: _id,
-                }
-            }, (err, result) => {
-                if(err || result.matchedCount != 1) return res.status(400).send({message: 'Error to remove bookmark'})
-                res.status(201).send(false)
-            })
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
+        const seriesId = parseInt(req.query.seriesId)
+        const _id = new ObjectId(req.user_id)
+        const result = await db.collection('series').updateOne({ seriesId }, {
+            $pull: {
+                subscribe: _id,
+            }
         })
-    } catch (error) {
-        res.status(500).send({message: 'This service is not available'})
+        if(result.modifiedCount === 1){
+            res.status(201).send(false)
+        } else {
+            res.status(500).send({message: 'Cannot subscribe', err})
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 
 exports.review = async (req, res) => {
-    try {
-        var db = mongoUtil.getDb()
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const { productId, review, score } = req.body
         const _id = new ObjectId(req.user_id)
-        db.collection('review').insertOne({
+        await db.collection('review').insertOne({
             productId,
             user_id: _id,
             review,
@@ -316,7 +352,7 @@ exports.review = async (req, res) => {
             }
         })
         const avg = Math.round(((product.score.avg * product.score.count) + score) / (product.score.count +1) *100)/100
-        db.collection('products').updateOne({productId}, {
+        await db.collection('products').updateOne({productId}, {
             $set : {
                 'score.avg' : avg
             },
@@ -324,26 +360,19 @@ exports.review = async (req, res) => {
                 'score.count': 1
             }
         })
-        // db.collection('review').updateOne({ productId }, {
-        //     $set : {
-        //         productId,
-        //     },
-        //     $push : {
-        //         list: {
-        //             user_id: _id,
-        //             review,
-        //             score
-        //         }
-        //     }
-        // }, { upsert : true })
-    } catch (error) {
-        
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
 
 exports.getReview = async (req, res) => {
-    try {
-        var db = mongoUtil.getDb()
+    const client = new MongoClient(process.env.MONGODB_URI)
+    try{
+        await client.connect()
+        const db = client.db(process.env.DB_NAME)
         const pid = req.query.productId
         const data = await db.collection('review').aggregate([{
                 $match: {productId: parseInt(pid)}
@@ -366,9 +395,24 @@ exports.getReview = async (req, res) => {
                 }
             }])
             .toArray()
-
         res.send(data)
-    } catch (error) {
-        console.log(error)
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({message: 'This service not available', err})
+    } finally {
+        await client.close()
     }
 }
+
+
+// const client = new MongoClient(process.env.MONGODB_URI)
+// try{
+//     await client.connect()
+//     const db = client.db(process.env.DB_NAME)
+
+// } catch (err) {
+//     console.log(err)
+//     res.status(500).send({message: 'This service not available', err})
+// } finally {
+//     await client.close()
+// }
