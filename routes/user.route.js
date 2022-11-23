938de4508586a1693aa2d09ff9bb382998e39711
Shinoo <s6203051623081@email.kmutnb.ac.th>
2022-11-24 00:13:43 +0700

    omise (curl)

--- routes/user.route.js
+++ routes/user.route.js
@@ -20,4 +20,34 @@ router.post('/placeOrder', authJwt.verifyToken, controller.placeOrder)
 router.get('/getSubscribes', authJwt.verifyToken, controller.getSubscribes)
 router.get('/getWishlists', authJwt.verifyToken, controller.getWishlists)
 
+const request = require('request')
+router.post('/omise', async (req, res) => {
+    var headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
+    
+    var dataString = 'amount=10000&currency=thb&card=' + req.body.token;
+    
+    var options = {
+        url: 'https://api.omise.co/charges',
+        method: 'POST',
+        headers: headers,
+        body: dataString,
+        auth: {
+            'user': 'skey_test_5tphsxg5lj1j0k7uojy',
+            'pass': ''
+        }
+    };
+    function callback(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        } else if(!error) {
+            console.log(body);
+        }
+    }
+    
+    request(options, callback);
+    res.send('ok')
+})
+
 module.exports = router
